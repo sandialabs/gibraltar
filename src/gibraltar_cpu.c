d88f178b69569e51f96f10ff6d4949b341f53d00
Matthew L. Curry <mlcurry@sandia.gov>
2014-10-23 20:38:41 -0600

    Cleanup

--- src/gibraltar_cpu.c
+++ src/gibraltar_cpu.c
@@ -1,9 +1,9 @@
 /* Author:  Matthew Curry
- * Email:   curryml@cis.uab.edu
+ * Email:   mlcurry@sandia.gov
  *
- * Gibraltar contains a low-performance CPU failback implementation.  This can
- * be used instead of the GPU implementation by compiling this file instead of
- * gib_cuda_rt.c
+ * Gibraltar contains a low-performance CPU failback implementation.
+ * This can be used instead of the GPU implementation by compiling
+ * this file instead of gib_cuda_driver.c
  */
 
 #include "../inc/gibraltar.h"
@@ -12,38 +12,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int gib_init ( int n, int m, gib_context *c ) {
-  return gib_cpu_init(n, m, c);
+int
+gib_init(int n, int m, gib_context *c)
+{
+	return gib_cpu_init(n, m, c);
 }
 
-int gib_destroy ( gib_context c ) {
-  return gib_cpu_destroy(c);
+int
+gib_destroy(gib_context c)
+{
+	return gib_cpu_destroy(c);
 }
 
-int gib_alloc ( void **buffers, int buf_size, int *ld, gib_context c ) {
-  return gib_cpu_alloc(buffers, buf_size, ld, c);
+int
+gib_alloc(void **buffers, int buf_size, int *ld, gib_context c)
+{
+	return gib_cpu_alloc(buffers, buf_size, ld, c);
 }
 
-int gib_free ( void *buffers, gib_context c ) {
-  return gib_cpu_free(buffers);
+int
+gib_free(void *buffers, gib_context c)
+{
+	return gib_cpu_free(buffers);
 }
 
-int gib_generate ( void *buffers, int buf_size, gib_context c ) {
-  return gib_cpu_generate(buffers, buf_size, c);
+int
+gib_generate(void *buffers, int buf_size, gib_context c)
+{
+	return gib_cpu_generate(buffers, buf_size, c);
 }
 
-int gib_generate_nc ( void *buffers, int buf_size, int work_size,  
-		      gib_context c) {
-  return gib_cpu_generate_nc(buffers, buf_size, work_size, c);
+int
+gib_generate_nc(void *buffers, int buf_size, int work_size,
+		gib_context c)
+{
+	return gib_cpu_generate_nc(buffers, buf_size, work_size, c);
 }
 
-int gib_recover ( void *buffers, int buf_size, int *buf_ids, int recover_last,
-		  gib_context c ) {
-  return gib_cpu_recover(buffers, buf_size, buf_ids, recover_last, c);
+int
+gib_recover(void *buffers, int buf_size, int *buf_ids, int recover_last,
+	    gib_context c)
+{
+	return gib_cpu_recover(buffers, buf_size, buf_ids, recover_last, c);
 }
 
-int gib_recover_nc ( void *buffers, int buf_size, int work_size, int *buf_ids, int recover_last,
-		     gib_context c ) {
-  return gib_cpu_recover_nc(buffers, buf_size, work_size, buf_ids, 
-			    recover_last, c);
+int
+gib_recover_nc(void *buffers, int buf_size, int work_size, int *buf_ids,
+	       int recover_last, gib_context c)
+{
+	return gib_cpu_recover_nc(buffers, buf_size, work_size, buf_ids,
+				  recover_last, c);
 }
