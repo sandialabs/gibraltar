45ccc19a91cfe1257cf1ccb45de91c48f152428f
Matthew L. Curry <mlcurry@sandia.gov>
2014-12-22 15:41:33 -0700

    Fix indentation, fix cpp symbol so it starts with _GIB, extraneous whitespace, 80-col restriction

--- inc/dynamic_fp.h
+++ inc/dynamic_fp.h
@@ -2,29 +2,31 @@
  *
  * Copyright (C) University of Alabama at Birmingham and Sandia
  * National Laboratories, 2010, written by and Rodrigo A. Sardinas
- * <ras0054@tigermail.auburn.edu>, under contract to Sandia National Laboratories.
+ * <ras0054@tigermail.auburn.edu>, under contract to Sandia National
+ * Laboratories.
  *
  * Changes:
  */
 
-#ifndef DYNAMIC_FP_
-#define DYNAMIC_FP_
+#ifndef _GIB_DYNAMIC_FP_H_
+#define _GIB_DYNAMIC_FP_H_
 
 struct gib_context_t;
 
 struct dynamic_fp {
-
-int (*gib_destroy)(struct gib_context_t *c);
-int (*gib_alloc)(void **buffers, int buf_size, int *ld, struct gib_context_t *c);
-int (*gib_free)(void *buffers, struct gib_context_t *c);
-int (*gib_generate)(void *buffers, int buf_size, struct gib_context_t *c);
-int (*gib_generate_nc)(void *buffers, int buf_size, int work_size,
-		    struct gib_context_t *c);
-int (*gib_recover)(void *buffers, int buf_size, int *buf_ids, int recover_last,
-		struct gib_context_t *c);
-int (*gib_recover_nc)(void *buffers, int buf_size, int work_size, int *buf_ids,
-		int recover_last, struct gib_context_t *c);
-
+	int (*gib_destroy)(struct gib_context_t *c);
+	int (*gib_alloc)(void **buffers, int buf_size, int *ld,
+			 struct gib_context_t *c);
+	int (*gib_free)(void *buffers, struct gib_context_t *c);
+	int (*gib_generate)(void *buffers, int buf_size,
+			    struct gib_context_t *c);
+	int (*gib_generate_nc)(void *buffers, int buf_size, int work_size,
+			       struct gib_context_t *c);
+	int (*gib_recover)(void *buffers, int buf_size, int *buf_ids,
+			   int recover_last, struct gib_context_t *c);
+	int (*gib_recover_nc)(void *buffers, int buf_size, int work_size,
+			      int *buf_ids, int recover_last,
+			      struct gib_context_t *c);
 };
 
 extern struct dynamic_fp cuda, jerasure, cpu;
